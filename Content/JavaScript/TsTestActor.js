f9cc0796a1d0037e6dbbaae6f5f3fd4f4dfe6a12
johnche <johnche@tencent.com>
2020-12-24 18:01:18 +0800

    更新例子

--- Content/JavaScript/TsTestActor.js
+++ Content/JavaScript/TsTestActor.js
@@ -1,27 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const UE = require("ue");
-console.error("Script init of TsTestActor ");
+console.warn("Script init of TsTestActor ");
 class TsTestActor extends UE.Actor {
     //注意，继承UE类的js类，构造函数必须大写开头
     Constructor() {
         this.PrimaryActorTick.bCanEverTick = true;
-        console.error("Constructor this.Add", this.Add(33, 44));
+        console.warn("TsTestActor.Constructor this.Add", this.Add(33, 44));
         this.tickCount = 0;
     }
     //override 1
     ReceiveBeginPlay() {
-        console.error("ReceiveBeginPlay");
-        console.error("ReceiveBeginPlay this.Add", this.Add(55, 66));
+        console.warn("TsTestActor.ReceiveBeginPlay this.Add", this.Add(55, 66));
     }
     Add(a, b) {
         return a + b;
     }
     //override 2
     ReceiveTick(DeltaSeconds) {
-        console.error("MyActor.ReceiveTick", DeltaSeconds, this.tickCount);
-        if (this.tickCount % 100 == 0 && this.tickCount <= 200) {
-            console.error("MyActor.ReceiveTick", DeltaSeconds, this.tickCount);
+        if (this.tickCount % 100 == 0) {
+            console.warn("TsTestActor.ReceiveTick", DeltaSeconds, this.tickCount);
         }
         ++this.tickCount;
     }
--- TsProj/TsTestActor.ts
+++ TsProj/TsTestActor.ts
@@ -1,6 +1,6 @@
 import * as UE from 'ue'
 
-console.error("Script init of TsTestActor ");
+console.warn("Script init of TsTestActor ");
 
 class TsTestActor extends UE.Actor {
     tickCount: number;
@@ -8,14 +8,13 @@ class TsTestActor extends UE.Actor {
     //注意，继承UE类的js类，构造函数必须大写开头
     Constructor() {
         this.PrimaryActorTick.bCanEverTick = true;
-        console.error("Constructor this.Add", this.Add(33, 44));
+        console.warn("TsTestActor.Constructor this.Add", this.Add(33, 44));
         this.tickCount = 0;
     }
 
     //override 1
     ReceiveBeginPlay(): void {
-        console.error("ReceiveBeginPlay");
-        console.error("ReceiveBeginPlay this.Add", this.Add(55, 66));
+        console.warn("TsTestActor.ReceiveBeginPlay this.Add", this.Add(55, 66));
     }
 
     Add(a: number, b: number): number {
@@ -24,9 +23,8 @@ class TsTestActor extends UE.Actor {
 
     //override 2
     ReceiveTick(DeltaSeconds: number): void {
-        console.error("MyActor.ReceiveTick", DeltaSeconds, this.tickCount);
-        if (this.tickCount % 100 == 0 && this.tickCount <= 200) {
-            console.error("MyActor.ReceiveTick", DeltaSeconds, this.tickCount);
+        if (this.tickCount % 100 == 0) {
+            console.warn("TsTestActor.ReceiveTick", DeltaSeconds, this.tickCount);
         }
         ++this.tickCount;
     }
