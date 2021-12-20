a534eb98b8509ddccf9b86b4d2f200407820b6eb
johnche <johnche@tencent.com>
2021-12-20 10:53:10 +0800

    esm演示

---
+++ Content/JavaScript/ESMTest.mjs
@@ -0,0 +1,12 @@
+import {abc} from 'a.mjs';
+import UE from "ue";
+
+console.error('es test..')
+
+abc();
+
+console.log(UE.EObjectTypeQuery.Player);
+console.log(UE.EObjectTypeQuery.Enemy);
+console.log(UE.EObjectTypeQuery.Projectile);
+console.log(UE.EObjectTypeQuery.WorldStatic);
+console.log(UE.ETraceTypeQuery.Weapon);
---
+++ Content/JavaScript/a.mjs
@@ -0,0 +1,3 @@
+export function abc() {
+    console.log('abc invoked...');
+}
