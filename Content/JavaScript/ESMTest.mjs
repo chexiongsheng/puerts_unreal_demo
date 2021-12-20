27700792a08bd78077c8882b171fc88270f85103
johnche <johnche@tencent.com>
2021-12-20 11:37:08 +0800

    esm例子

--- Content/JavaScript/ESMTest.mjs
+++ Content/JavaScript/ESMTest.mjs
@@ -1,12 +1,9 @@
 import {abc} from 'a.mjs';
+import {def} from 'b.mjs';
 import UE from "ue";
 
 console.error('es test..')
 
 abc();
-
-console.log(UE.EObjectTypeQuery.Player);
-console.log(UE.EObjectTypeQuery.Enemy);
-console.log(UE.EObjectTypeQuery.Projectile);
-console.log(UE.EObjectTypeQuery.WorldStatic);
-console.log(UE.ETraceTypeQuery.Weapon);
+def();
+console.log("UE.EObjectTypeQuery.WorldStatic", UE.EObjectTypeQuery.WorldStatic);
---
+++ Content/JavaScript/b.mjs
@@ -0,0 +1,6 @@
+import {abc} from 'a.mjs';
+
+export function def() {
+    console.log('def invoked...');
+    abc();
+}
