f458cb4ca7804da928ab2157bef2af1597f7650b
johnche <johnche@tencent.com>
2023-05-31 17:06:51 +0800

    重构UT

--- Content/JavaScript/RunCaseTest.js
+++ Content/JavaScript/RunCaseTest.js
@@ -1,35 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("puerts");
-;
-;
-let mochaStr = puerts.loadModule("mocha", "../JavaScript/mocha.js");
-mochaStr = mochaStr.substring(mochaStr.indexOf('\n') + 1).substring(mochaStr.indexOf('\n') + 1);
-let executeScript = mochaStr
-    + " mocha.setup({ "
-    + "   ui: 'bdd', "
-    + "   reporter: 'json' "
-    + " }); "
-    + " mocha.addFile('./CaseTest.js'); "
-    + " mocha.run().on('end', function() { "
-    + "     let allTestsNum = this.testResults.stats.tests; "
-    + "     let passesNum = this.testResults.stats.passes; "
-    + "     let failuresNum = this.testResults.stats.failures; "
-    + "     if (failuresNum > 0) {"
-    + "         console.log(JSON.stringify(this.testResults.failures, null, 4));"
-    + "     }"
-    + "     console.warn(`Test Summary: Pass[${passesNum}/${allTestsNum}], Fail[${failuresNum}/${allTestsNum}].`); "
-    + " }); ";
+require('mocha');
 class BOMString {
     replace(regex, replacer) {
         return this;
     }
 }
-global.location = new BOMString;
-function wrapCode(src) {
-    src = '{' + src + '}';
-    const code = new Function('console', 'global', 'require', src);
-    return code;
-}
-wrapCode(executeScript)(console, global, globalThis.require);
+globalThis.location = new BOMString;
+mocha.setup({
+    ui: 'bdd',
+    reporter: 'json'
+});
+console.log("start run test case");
+mocha.addFile('./CaseTest.js');
+mocha.run().on('end', function () {
+    let allTestsNum = this.testResults.stats.tests;
+    let passesNum = this.testResults.stats.passes;
+    let failuresNum = this.testResults.stats.failures;
+    if (failuresNum > 0) {
+        console.log(JSON.stringify(this.testResults.failures, null, 4));
+    }
+    console.warn(`Test Summary: Pass[${passesNum}/${allTestsNum}], Fail[${failuresNum}/${allTestsNum}].`);
+});
 //# sourceMappingURL=RunCaseTest.js.map
--- TsProj/RunCaseTest.ts
+++ TsProj/RunCaseTest.ts
@@ -1,34 +1,6 @@
-import 'puerts';
+declare function require(name: string): any;
 
-declare class puerts {
-    static loadModule(moduleName: string, modulePath: string): string;
-
-    static console: any;
-};
-
-declare class global {
-    static location: any;
-};
-
-let mochaStr = puerts.loadModule("mocha", "../JavaScript/mocha.js");
-
-mochaStr = mochaStr.substring(mochaStr.indexOf('\n') + 1).substring(mochaStr.indexOf('\n') + 1);
-
-let executeScript = mochaStr
-    + " mocha.setup({ "
-    + "   ui: 'bdd', "
-    + "   reporter: 'json' "
-    + " }); "
-    + " mocha.addFile('./CaseTest.js'); "
-    + " mocha.run().on('end', function() { "
-    + "     let allTestsNum = this.testResults.stats.tests; "
-    + "     let passesNum = this.testResults.stats.passes; "
-    + "     let failuresNum = this.testResults.stats.failures; "
-    + "     if (failuresNum > 0) {"
-    + "         console.log(JSON.stringify(this.testResults.failures, null, 4));"
-    + "     }"
-    + "     console.warn(`Test Summary: Pass[${passesNum}/${allTestsNum}], Fail[${failuresNum}/${allTestsNum}].`); "
-    + " }); ";
+require('mocha');
 
 class BOMString {
     replace(regex: string, replacer: string): BOMString {
@@ -36,12 +8,24 @@ class BOMString {
     }
 }
 
-global.location = new BOMString;
+(globalThis as any).location = new BOMString;
 
-function wrapCode(src: string) {
-    src = '{' + src + '}';
-    const code = new Function('console', 'global', 'require', src);
-    return code;
-}
+mocha.setup({
+    ui: 'bdd',
+    reporter: 'json'
+}); 
 
-wrapCode(executeScript)(console, global, (globalThis as any).require);
+console.log("start run test case");
+
+
+mocha.addFile('./CaseTest.js');
+mocha.run().on('end', function() {
+     let allTestsNum = this.testResults.stats.tests; 
+     let passesNum = this.testResults.stats.passes; 
+     let failuresNum = this.testResults.stats.failures; 
+     if (failuresNum > 0) {
+         console.log(JSON.stringify(this.testResults.failures, null, 4));
+     }
+     console.warn(`Test Summary: Pass[${passesNum}/${allTestsNum}], Fail[${failuresNum}/${allTestsNum}].`);
+ });
+ 
