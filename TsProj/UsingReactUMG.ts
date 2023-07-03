c447ffce83bc2ad22defd9e6aefc35e7ddc48f70
johnche <johnche@tencent.com>
2023-07-03 14:34:38 +0800

    更改ts目录

--- TsProj/UsingReactUMG.ts
+++
@@ -1,9 +0,0 @@
-import * as UE from 'ue'
-import {argv} from 'puerts';
-import {ReactUMG} from 'react-umg';
-import * as UI from './main-ui';
-
-//用React来写UI
-let world = (argv.getByName("GameInstance") as UE.GameInstance).GetWorld();
-ReactUMG.init(world);
-UI.Load();
