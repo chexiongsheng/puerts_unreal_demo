7fdd37786934bca9e313526da59e03a99b5db93b
johnche <johnche@tencent.com>
2023-10-17 11:51:33 +0800

    update plugin

---
+++ Plugins/Puerts/Source/JsEnv/Public/NamespaceDef.h
@@ -0,0 +1,16 @@
+/*
+ * Tencent is pleased to support the open source community by making Puerts available.
+ * Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+ * Puerts is licensed under the BSD 3-Clause License, except for the third-party components listed in the file 'LICENSE' which may
+ * be subject to their corresponding license terms. This file is subject to the terms and conditions defined in file 'LICENSE',
+ * which is part of this source code package.
+ */
+
+#pragma once
+
+#if defined(WITH_QJS_NAMESPACE_SUFFIX)
+#define PUERTS_NAMESPACE puerts_qjs
+namespace v8 = v8_qjs;
+#else
+#define PUERTS_NAMESPACE puerts
+#endif
